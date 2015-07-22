e15b894a76c73c571b204035d876a602efc433f2
ergl <ergl@users.noreply.github.com>
2015-07-22 17:24:59 +0200

    loader_spec: rephrase test description

--- spec/loader_spec.py
+++ spec/loader_spec.py
@@ -4,13 +4,13 @@ from spec.custom_matchers.contain_exactly_function_called import contain_exactly
 
 from pysellus import loader
 
-with description('the loader module loads all functions in the top level of each file in the passed directory'):
-    with it('when there is only one file'):
+with description('the loader module loads all top-level functions in a directory'):
+    with it('should load every function when there is only one file'):
             expect(loader.load('spec/fixtures/one_file/')).to(
                 contain_exactly_function_called('a_function', 'another_function')
             )
 
-    with it('when there is more than one file'):
+    with it('should load every function  when there is more than one file'):
         expect(loader.load('spec/fixtures/multiple_files/')).to(
             contain_exactly_function_called('file_1_function_a',
                                             'file_1_function_b',
