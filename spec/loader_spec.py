9706a9d34f3bf71f1021f42beb35ea2c8e82c4b9
ergl <ergl@users.noreply.github.com>
2015-08-03 02:25:13 +0200

    Add option to launch pysellus from a single file
    New options allows to test the program with a single file without having
    to rename all files in the parent directory to __file.
    
    Loader now tests if the given path is a python file (ends with .py).
    If it does, import it and get all pscheck functions from it in the same
    way we did with directories.
    
    To avoid duplication, the process of loading the check functions from a
    module has been moved to its own function: `_get_checks_from_module`
    
    Tests have been modified to add a new test case for the new
    functionality, and changed old descriptions.

--- spec/loader_spec.py
+++ spec/loader_spec.py
@@ -4,14 +4,20 @@ from spec.custom_matchers.contain_exactly_function_called import contain_exactly
 
 from pysellus import loader
 
-with description('the loader module loads all top-level functions in a directory'):
+with description('the loader module loads all top-level functions in a directory or file'):
     with it('should load every function when there is only one file'):
         expect(loader.load('spec/fixtures/one_file/')).to(
             contain_exactly_function_called('pscheck_a_function',
                                             'pscheck_another_function')
         )
 
-    with it('should load every function  when there is more than one file'):
+    with it('should load every function of the specified file'):
+        expect(loader.load('spec/fixtures/multiple_files/file_1.py')).to(
+            contain_exactly_function_called('pscheck_file_1_function_a',
+                                            'pscheck_file_1_function_b')
+        )
+
+    with it('should load every function when there is more than one file'):
         expect(loader.load('spec/fixtures/multiple_files/')).to(
             contain_exactly_function_called('pscheck_file_1_function_a',
                                             'pscheck_file_1_function_b',
@@ -21,7 +27,7 @@ with description('the loader module loads all top-level functions in a directory
                                             'pscheck_file_3_function_b')
         )
 
-    with it("shouldn't load private functions (starting with underscore)"):
+    with it("should only load setup functions (starting with pscheck)"):
         expect(loader.load('spec/fixtures/file_with_helper_functions/')).to(
             contain_exactly_function_called('pscheck_function')
         )
