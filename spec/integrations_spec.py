86f0ddd561f7d1ec59a34a6b6d7d187b95c30f89
Ángel Sanz <angelsanzgit@gmail.com>
2015-08-06 11:40:45 +0200

    Add terminal integration test

--- spec/integrations_spec.py
+++ spec/integrations_spec.py
@@ -1,4 +1,6 @@
-from expects import expect, be
+import rx
+
+from expects import expect, be, contain_exactly, be_a
 from doublex import Spy
 from doublex_expects import have_been_called
 
@@ -17,11 +19,22 @@ with description('the integrations module'):
         with it('returns the decorated function as is'):
             decorated_function = Spy().decorated_function
 
-            expect(on_failure('some_integration_name')(decorated_function)).to(be(decorated_function))
+            expect(on_failure('terminal')(decorated_function)).to(be(decorated_function))
 
         with it('doesn\'t call the decorated function'):
             decorated_function = Spy().decorated_function
 
-            on_failure('some_integration_name')(decorated_function)
+            on_failure('terminal')(decorated_function)
 
             expect(decorated_function).to_not(have_been_called)
+
+        with it('has the (convenient) side effect of registering the integration name with a subject'):
+            decorated_function = Spy().decorated_function
+
+            on_failure('terminal')(decorated_function)
+
+            expect(list(integrations.integrations.keys())).to(contain_exactly(decorated_function.__name__))
+
+            for list_of_associated_subjects in integrations.integrations.values():
+                for subject in list_of_associated_subjects:
+                    expect(subject).to(be_a(rx.subjects.Subject))
