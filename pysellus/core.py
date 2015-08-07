fc34847bb9bbe74f4b909342872275ecb652bbaa
ergl <ergl@users.noreply.github.com>
2015-08-07 11:49:08 +0200

    core: Fixed whitespace

--- pysellus/core.py
+++ pysellus/core.py
@@ -3,7 +3,6 @@ import argparse
 from pysellus import loader, registrar, threader
 
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('directory', type=str,
