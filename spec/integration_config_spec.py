a98eabe0e14ca7263bfb873b7ad9d1f50e21f9c7
Ángel Sanz <angelsanzgit@gmail.com>
2015-10-18 13:24:15 +0200

    integration_config: add tests supporting integrations without parameters

--- spec/integration_config_spec.py
+++ spec/integration_config_spec.py
@@ -12,39 +12,71 @@ with description('the integration_config module'):
             integration_config._get_integration_instance = self.integration_config_spy._get_integration_instance
 
         with context('when an integration alias is specified'):
-            with it('requests an integration instance and registers that alias'):
-                kwargs_for_integration_constructor = {
-                    'some_arg': 'some_value',
-                    'another_arg': 35
-                }
-                integrations_configuration = {
-                    'my-alias': {
-                        'an_integration': kwargs_for_integration_constructor
+            with context('and the integration is configured with one or more parameters'):
+                with it('requests an integration instance and registers that alias'):
+                    kwargs_for_integration_constructor = {
+                        'some_arg': 'some_value',
+                        'another_arg': 35
                     }
-                }
+                    integrations_configuration = {
+                        'my-alias': {
+                            'an_integration': kwargs_for_integration_constructor
+                        }
+                    }
 
-                integration_config._load_integrations_from_configuration(integrations_configuration)
+                    integration_config._load_integrations_from_configuration(integrations_configuration)
 
-                expect(self.integration_config_spy._get_integration_instance).to(
-                    have_been_called_with('an_integration', kwargs_for_integration_constructor).once
-                )
-                expect(integration_config.loaded_integrations).to(have_key('my-alias'))
+                    expect(self.integration_config_spy._get_integration_instance).to(
+                        have_been_called_with('an_integration', kwargs_for_integration_constructor).once
+                    )
+                    expect(integration_config.loaded_integrations).to(have_key('my-alias'))
+
+            with context('and the integration is configured with no parameters'):
+                with it('requests an integration instance and registers that alias'):
+                    kwargs_for_integration_constructor = None
+                    integrations_configuration = {
+                        'my-alias': {
+                            'an_integration': kwargs_for_integration_constructor
+                        }
+                    }
+
+                    integration_config._load_integrations_from_configuration(integrations_configuration)
+
+                    expect(self.integration_config_spy._get_integration_instance).to(
+                        have_been_called_with('an_integration', kwargs_for_integration_constructor).once
+                    )
+                    expect(integration_config.loaded_integrations).to(have_key('my-alias'))
 
         with context('when an integration alias is not specified'):
-            with it('requests an integration instance and registers the stock name'):
-                kwargs_for_integration_constructor = {
-                    'some_arg': 'some_value'
-                }
-                integrations_configuration = {
-                    'an_integration': kwargs_for_integration_constructor
-                }
+            with context('and the integration is configured with one or more parameters'):
+                with it('requests an integration instance and registers the stock name'):
+                    kwargs_for_integration_constructor = {
+                        'some_arg': 'some_value'
+                    }
+                    integrations_configuration = {
+                        'an_integration': kwargs_for_integration_constructor
+                    }
 
-                integration_config._load_integrations_from_configuration(integrations_configuration)
+                    integration_config._load_integrations_from_configuration(integrations_configuration)
 
-                expect(self.integration_config_spy._get_integration_instance).to(
-                    have_been_called_with('an_integration', kwargs_for_integration_constructor).once
-                )
-                expect(integration_config.loaded_integrations).to(have_key('an_integration'))
+                    expect(self.integration_config_spy._get_integration_instance).to(
+                        have_been_called_with('an_integration', kwargs_for_integration_constructor).once
+                    )
+                    expect(integration_config.loaded_integrations).to(have_key('an_integration'))
+
+            with context('and the integration is configured with no parameters'):
+                with it('requests an integration instance and registers the stock name'):
+                    kwargs_for_integration_constructor = None
+                    integrations_configuration = {
+                        'an_integration': kwargs_for_integration_constructor
+                    }
+
+                    integration_config._load_integrations_from_configuration(integrations_configuration)
+
+                    expect(self.integration_config_spy._get_integration_instance).to(
+                        have_been_called_with('an_integration', kwargs_for_integration_constructor).once
+                    )
+                    expect(integration_config.loaded_integrations).to(have_key('an_integration'))
 
         with after.each:
             integration_config._get_integration_instance = self.original_integration_instance_creator
