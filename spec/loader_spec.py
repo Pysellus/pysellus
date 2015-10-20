ab4f6082210488f31ada5fd3d58c506da1b4fcd7
ergl <ergl@users.noreply.github.com>
2015-10-20 11:14:04 +0200

    loader.load -> loader.load_test_files, add documentation

--- spec/loader_spec.py
+++ spec/loader_spec.py
@@ -6,19 +6,19 @@ from pysellus import loader
 
 with description('the loader module loads all top-level functions in a directory or file'):
     with it('should load every function when there is only one file'):
-        expect(loader.load('spec/fixtures/one_file/')).to(
+        expect(loader.load_test_files('spec/fixtures/one_file/')).to(
             contain_exactly_function_called('a_function',
                                             'another_function')
         )
 
     with it('should load every function of the specified file'):
-        expect(loader.load('spec/fixtures/multiple_files/file_1.py')).to(
+        expect(loader.load_test_files('spec/fixtures/multiple_files/file_1.py')).to(
             contain_exactly_function_called('file_1_function_a',
                                             'file_1_function_b')
         )
 
     with it('should load every function when there is more than one file'):
-        expect(loader.load('spec/fixtures/multiple_files/')).to(
+        expect(loader.load_test_files('spec/fixtures/multiple_files/')).to(
             contain_exactly_function_called('file_1_function_a',
                                             'file_1_function_b',
                                             'file_2_function_a',
@@ -28,6 +28,6 @@ with description('the loader module loads all top-level functions in a directory
         )
 
     with it("should only load setup functions"):
-        expect(loader.load('spec/fixtures/file_with_helper_functions/')).to(
+        expect(loader.load_test_files('spec/fixtures/file_with_helper_functions/')).to(
             contain_exactly_function_called('function')
         )
