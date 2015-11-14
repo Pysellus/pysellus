ddf2bfdb946b460db2e2759c1a02338ed72d801a
Ángel Sanz <angelsanzgit@gmail.com>
2015-11-14 10:11:57 +0100

    integration_config_spec: refactor a test to use a loop

--- spec/integration_config_spec.py
+++ spec/integration_config_spec.py
@@ -67,18 +67,12 @@ with description('the integration_config module'):
 
                 with context('each configuration contains a module path and a class name'):
                     with it('aborts the program if the either key is missing'):
-                        config_dict = {'some_alias': {}}
-                        config_dict_1 = {'some_alias': {'name': 'foo'}}
-                        config_dict_2 = {'some_alias': {'path': 'foo'}}
-                        expect(lambda: integration_config._load_custom_integrations_classes(config_dict)).to(
-                            raise_error(SystemExit)
-                        )
-                        expect(lambda: integration_config._load_custom_integrations_classes(config_dict_1)).to(
-                            raise_error(SystemExit)
-                        )
-                        expect(lambda: integration_config._load_custom_integrations_classes(config_dict_2)).to(
-                            raise_error(SystemExit)
-                        )
+                        for malformed_config_dict in [{'some_alias': {}},
+                                                      {'some_alias': {'name': 'foo'}},
+                                                      {'some_alias': {'path': 'foo'}}]:
+                            expect(lambda: integration_config._load_custom_integrations_classes(malformed_config_dict)).to(
+                                raise_error(SystemExit)
+                            )
 
                     with it('loads the module and finds the class name inside it'):
                         load_modules = loader.load_modules
